6ecfa6c31b62c607850adbdb5f2945f65495d1f7
陈欣 <admin@aadps.net>
2018-09-22 12:24:26 +0800

    Switch to APNS for iOS

--- ios/aadps/AppDelegate.m
+++ ios/aadps/AppDelegate.m
@@ -10,7 +10,6 @@
 #import "AppDelegate.h"
 
 #import "RCTRootView.h"
-#import "BPush.h"
 
 #define DBNAME    @"aadps.db"
 
@@ -67,18 +66,7 @@
     UIRemoteNotificationType myTypes = UIRemoteNotificationTypeBadge|UIRemoteNotificationTypeAlert|UIRemoteNotificationTypeSound;
     [[UIApplication sharedApplication] registerForRemoteNotificationTypes:myTypes];
   }
-  
-  [BPush registerChannel:launchOptions apiKey:@"MwbVNFcqWFNq3PRAZIfD6owE" pushMode:BPushModeProduction withFirstAction:nil withSecondAction:nil withCategory:nil useBehaviorTextInput:YES isDebug:NO];
-  
-  // 禁用地理位置推送 需要再绑定接口前调用。
-  
-  [BPush disableLbs];
-  
-  // App 是用户点击推送消息启动
-  NSDictionary *userInfo = [launchOptions objectForKey:UIApplicationLaunchOptionsRemoteNotificationKey];
-  if (userInfo) {
-    [BPush handleNotification:userInfo];
-  }
+
 #if TARGET_IPHONE_SIMULATOR
   Byte dt[32] = {0xc6, 0x1e, 0x5a, 0x13, 0x2d, 0x04, 0x83, 0x82, 0x12, 0x4c, 0x26, 0xcd, 0x0c, 0x16, 0xf6, 0x7c, 0x74, 0x78, 0xb3, 0x5f, 0x6b, 0x37, 0x0a, 0x42, 0x4f, 0xe7, 0x97, 0xdc, 0x9f, 0x3a, 0x54, 0x10};
   [self application:application didRegisterForRemoteNotificationsWithDeviceToken:[NSData dataWithBytes:dt length:32]];
@@ -86,6 +74,11 @@
   //角标清0
   [[UIApplication sharedApplication] setApplicationIconBadgeNumber:0];
   return YES;
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+  [[UIApplication sharedApplication] setApplicationIconBadgeNumber:0];
 }
 
 - (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings
@@ -98,63 +91,42 @@
 
 - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
 {
-  NSLog(@"test:%@",deviceToken);
-  [BPush registerDeviceToken:deviceToken];
-  [BPush bindChannelWithCompleteHandler:^(id result, NSError *error) {
-
-    // 需要在绑定成功后进行 settag listtag deletetag unbind 操作否则会失败
-    
-    // 网络错误
-    if (error) {
-      return ;
-    }
-    if (result) {
-      // 确认绑定成功
-      if ([result[@"error_code"]intValue]!=0) {
-        return;
-      }
-      // 获取channel_id
-      NSString *myChannel_id = [BPush getChannelId];
-      NSLog(@"==%@",myChannel_id);
-      NSArray *paths = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES);
-      NSString *library_path = [paths objectAtIndex:0];
-      NSString *database_path = [[library_path stringByAppendingString:@"/LocalDatabase"] stringByAppendingPathComponent:DBNAME];
+  NSLog(@"Device token: %@",deviceToken);
+  NSArray *paths = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES);
+  NSString *library_path = [paths objectAtIndex:0];
+  NSString *database_path = [[library_path stringByAppendingString:@"/LocalDatabase"] stringByAppendingPathComponent:DBNAME];
+  
+  if (sqlite3_open([database_path UTF8String], &db) != SQLITE_OK) {
+    sqlite3_close(db);
+    NSLog(@"数据库打开失败");
+  }else{
+    NSLog(@"数据库打开成功");
+    char *error;
+    sqlite3_exec(db, "DELETE FROM options WHERE id = 4", NULL, NULL, &error);
+    sqlite3_exec(db, [[NSString stringWithFormat:@"%@%@%@", @"INSERT INTO options VALUES(4, \"", deviceToken, @"\")"]  cStringUsingEncoding:NSASCIIStringEncoding], NULL, NULL, &error);
+    NSString  * query = @"SELECT * from options";
+    sqlite3_stmt* stmt =NULL;
+    sqlite3_prepare_v2(db, [query UTF8String], -1, &stmt, NULL);
+    while (sqlite3_step(stmt) == SQLITE_ROW) //get each row in loop
+    {
+      NSInteger age =  sqlite3_column_int(stmt, 0);
+      NSString * name =@"";
+      if(sqlite3_column_text(stmt, 1)!=NULL)name =[NSString stringWithUTF8String:(const char *)sqlite3_column_text(stmt, 1)];
       
-      if (sqlite3_open([database_path UTF8String], &db) != SQLITE_OK) {
-        sqlite3_close(db);
-        NSLog(@"数据库打开失败");
-      }else{
-        NSLog(@"数据库打开成功");
-        char *error;
-        sqlite3_exec(db, "DELETE FROM options WHERE id = 4", NULL, NULL, &error);
-        sqlite3_exec(db, [[NSString stringWithFormat:@"%@%@%@", @"INSERT INTO options VALUES(4, \"", myChannel_id, @"\")"]  cStringUsingEncoding:NSASCIIStringEncoding], NULL, NULL, &error);
-        NSString  * query = @"SELECT * from options";
-        sqlite3_stmt* stmt =NULL;
-        sqlite3_prepare_v2(db, [query UTF8String], -1, &stmt, NULL);
-        while (sqlite3_step(stmt) == SQLITE_ROW) //get each row in loop
-        {
-                    NSInteger age =  sqlite3_column_int(stmt, 0);
-          NSString * name =@"";
-          if(sqlite3_column_text(stmt, 1)!=NULL)name =[NSString stringWithUTF8String:(const char *)sqlite3_column_text(stmt, 1)];
-
-          
-          NSLog(@"选项%ld：%@", (long)age, name);
-          
-        }
-        NSLog(@"Done");
-        sqlite3_finalize(stmt);
-        sqlite3_prepare_v2(db, [query UTF8String], -1, &stmt, NULL);
-        sqlite3_close(db);
-      }
+      
+      NSLog(@"选项%ld：%@", (long)age, name);
+      
     }
-  }];
+    NSLog(@"Done");
+    sqlite3_finalize(stmt);
+    sqlite3_prepare_v2(db, [query UTF8String], -1, &stmt, NULL);
+    sqlite3_close(db);
+  }
 }
 
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
 {
 
-
-  
   NSLog(@"%@",userInfo);
   
   NSArray *paths = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES);
@@ -167,8 +139,9 @@
   }else{
     char *error;
     sqlite3_exec(db, "DELETE FROM options WHERE id = 5", NULL, NULL, &error);
-    sqlite3_exec(db, [[NSString stringWithFormat:@"%@%@%@", @"INSERT INTO options VALUES(5, \"", [userInfo objectForKey:@"view"], @"\")"]  cStringUsingEncoding:NSASCIIStringEncoding], NULL, NULL, &error);
+    sqlite3_exec(db, [[NSString stringWithFormat:@"%@%@%@", @"INSERT INTO options VALUES(5, \"", userInfo[@"view"], @"\")"]  cStringUsingEncoding:NSASCIIStringEncoding], NULL, NULL, &error);
     sqlite3_close(db);
+    NSLog(@"View saved: %@", userInfo[@"view"]);
   }
 }
 
